0b9712f272276a6c93ffe485eb90fa83db975297
newdream <newdream0730@gmail.com>
2022-09-16 19:11:37 +0200

    bug fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-<<<<<<< HEAD
 import react from '@astrojs/react';
 import netlify from '@astrojs/netlify/functions';
 
@@ -11,20 +10,4 @@ export default defineConfig({
   site: 'https://astro-project.netlify.app',
   base: '/gpm-lds-astro',
   clientEntrypoint: '@astrojs/react/client.js',
-=======
-import preact from '@astrojs/preact';
-import react from '@astrojs/react';
-
-// https://astro.build/config
-export default defineConfig({
-	integrations: [
-		// Enable Preact to support Preact JSX components.
-		preact(),
-		// Enable React for the Algolia search component.
-		react(),
-	],
-	// renderers: ['@astrojs/renderer-react'],
-	site: `https://newDream0730.github.io`,
-	// base: '/experiment-astro22H2-lds-react-vlaznim-mustafa'
->>>>>>> f504e447f718a9e93b04b204698361f67bef2abb
 });
